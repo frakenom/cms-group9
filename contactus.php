a1c463a688efdcc2961d8126b8a5c83c59b06c1d
unbiased0variable <cb0851@gmail.com>
2021-11-24 23:22:45 -0500

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -40,11 +40,29 @@
 ?>
 
 <form action="admin/insert_contact.php" method="POST">
-  Full Name : <input type="text" name="fullname" placeholder="Enter Full Name" Required>
-  <br/>
-  Age : <input type="text" name="age" placeholder="Enter Age" Required>
-  <br/>
-  <input type="submit" name="submit" value="Submit">
+    <label for="fullname">Full Name</label>
+    <input type="text" id="fullname" name="name" placeholder="Your Full Name" required>
+
+    <label for="email">E-mail</label>
+    <input type="email" id="email" name="email" placeholder="Your Email" required>
+
+    <label for="student">Student</label>
+    <select id="student" name="student">
+      <option value="current">Current</option>
+      <option value="new">New</option>
+    </select>
+
+    <label for="program">Student</label>
+    <select id="program" name="program">
+      <option value="dept1">Information Technology</option>
+      <option value="dept2">Creative Arts and Design</option>
+      <option value="dept3">Business Management</option>
+    </select>
+
+    <label for="subject">Please type in your question:</label>
+    <textarea id="subject" name="subject" style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
 </form>
 
 </body>
