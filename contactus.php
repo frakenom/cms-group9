cbe86c74d543c8d7d948ddde6349888b8102ed7c
unbiased0variable <cb0851@gmail.com>
2021-11-25 10:57:35 -0500

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -8,37 +8,51 @@
 </head>
 <body>
 <?php
-    include "admin/includes/database.php"; // Using database connection file here
+     $connect = mysqli_connect(
+        'sql208.epizy.com', //hostname
+        'epiz_30348373', //username
+        'gpwDiTjdJMBKqIr', //password
+        'epiz_30348373_cmsgroup9'); //database
 
-    if(isset($_POST['submit']))
-    {		
-        $fullname = $_POST['fullname'];
-        $email = $_POST['email'];
-        $student = $_POST['student'];
-        $program = $_POST['program'];
-        $message = $_POST['message'];
+    // Error message: If the connection didn't go through
 
-        $insert = mysqli_query($connect,"INSERT INTO `contact`(fullname, email, student, program, message) VALUES ('$fullname','$email', '$student', '$program', '$message')");
-        $result = mysqli_query($connect, $insert);
-
-        if(!$insert)
-        {
-            echo mysqli_error();
-        }
-        else
-        {
-            echo "Records added successfully.";
-        }
+    if (!$connect) 
+    {
+        echo 'Error Code: ' . mysqli_connect_errno() . '<br>';
+        echo 'Error Message: ' . mysqli_connect_error() . '<br>';
+        exit;
     }
 
-    mysqli_close($connect); // Close connection
+    // Taking all 5 values from the form data(input)
+
+    $fullname =  $_REQUEST['fullname'];
+    $email = $_REQUEST['email'];
+    $student =  $_REQUEST['student'];
+    $program = $_REQUEST['program'];
+    $message = $_REQUEST['message'];
+      
+    // Performing insert query execution
+    // here our table name is college
+    $sql = "INSERT INTO contact VALUES ('$fullname', 
+        '$email','$student','$program','$message')";
+      
+    if(mysqli_query($connect, $sql))
+    {
+        echo "Please fill the form if you have a question for us."; 
+    } else{
+        echo "ERROR: Hush! Sorry $sql. " 
+            . mysqli_error($connect);
+    }
+
+    // Close connection
+    mysqli_close($connect);
 ?>
 
-    <h3>Fill the Form</h3>
+    <h1>Contact Us</h2>
 
     <form method="POST">
         <label for="fullname">Full Name</label>
-        <input type="text" id="fullname" name="name" placeholder="Your Full Name" required>
+        <input type="text" id="fullname" name="fullname" placeholder="Your Full Name" required>
 
         <label for="email">E-mail</label>
         <input type="email" id="email" name="email" placeholder="Your Email" required>
@@ -56,11 +70,13 @@
         <option value="dept3">Business Management</option>
         </select>
 
-        <label for="subject">Please type in your question:</label>
-        <textarea id="subject" name="subject" style="height:200px"></textarea>
+        <label for="message">Please type in your question:</label>
+        <textarea id="message" name="message" style="height:200px" required></textarea>
 
         <input type="submit" value="Submit">
     </form>
+
+    
 
 </body>
 </html>
